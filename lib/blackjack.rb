e91e8ebda79d2c69fcaf9ebdd24c0046e118fc40
Jacob Tobias <jtobias811@gmail.com>
2020-11-30 16:15:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,10 +39,10 @@ def hit?
   # code hit? here
   prompt_user
   user_input = get_user_input
-  if user_input == "h"
-    card_total += deal_card
-  elsif user_input == "s"
+  if user_input == "s"
     card_total
+  elsif user_input == "h"
+    card_total +-= deal_card
   else
     invalid_command
   end
